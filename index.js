8f6d2ed5d9e7d2e5cc0d59242983cb2411d81828
Adrian Alberto <adrianalberto1986@gmail.com>
2022-05-09 16:47:01 -0500

    commit tree 7c757ecb18242f493bd0e253cfb8ad8b10014137

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function drivers(scrubDrivers){
+    const newScrubDrivers = scrubDrivers.slice(0,2)
+    return newScrubDrivers
+}
+
+const returnLastTwoDrivers = function drivers(scrubDrivers){
+    const newScrubDrivers = scrubDrivers.slice(-2)
+    return newScrubDrivers
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(multiplier){
+    function fareMultiplier(fare){
+        return fare * multiplier
+    }
+    return fareMultiplier
+}
+
+const fareDoubler = createFareMultiplier(2)
+const fareTripler = createFareMultiplier(3)
+
+function selectDifferentDrivers(arrayOfDrivers, callBack){
+    return callBack(arrayOfDrivers)
+}
+
